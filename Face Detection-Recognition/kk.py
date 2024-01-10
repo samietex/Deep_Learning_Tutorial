f25e89dc9b1bb061235718920fa0ddae338da0c3
samietex <shaibu.samuel@yahoo.com>
2024-01-10 16:17:39 +0100

    New commits

--- Face Detection-Recognition/kk.py
+++ Face Detection-Recognition/kk.py
@@ -4,7 +4,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # Read the input image
-img = cv2.imread('test.jpg')
+img = cv2.imread('supereagles.jpg')
 
 # Convert into grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
